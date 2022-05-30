d23df797a3b41b7a0fe57bbdc35c49952bebf79f
Haf <haf@protonmail.ch>
2022-05-30 13:47:06 +0200

    updated most dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,11 @@ val kotestVersion = "5.3.0"
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("com.h2database:h2:1.4.197")
-    implementation("com.sparkjava:spark-core:2.9.2")
-    implementation("org.json:json:20200518")
-    implementation("org.dalesbred:dalesbred:1.3.2")
-    implementation("org.slf4j:slf4j-simple:1.7.30")
+    implementation("com.h2database:h2:1.4.197")?.because("Can't update or tests break")
+    implementation("com.sparkjava:spark-core:2.9.3")
+    implementation("org.json:json:20220320")
+    implementation("org.dalesbred:dalesbred:1.3.5")
+    implementation("org.slf4j:slf4j-simple:1.7.36")
     implementation("org.kodein.di:kodein-di:7.11.0")
     implementation("com.google.guava:guava:31.1-jre")
 
