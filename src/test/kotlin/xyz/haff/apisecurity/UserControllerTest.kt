ae0236c6f65f475383c73d9e272f2572da41ae10
Haf <haf@protonmail.ch>
2022-06-05 12:53:54 +0200

    testing registration

--- src/test/kotlin/xyz/haff/apisecurity/UserControllerTest.kt
+++ src/test/kotlin/xyz/haff/apisecurity/UserControllerTest.kt
@@ -1,44 +1,77 @@
 package xyz.haff.apisecurity
 
-import io.kotest.assertions.throwables.shouldNotThrow
+import io.kotest.assertions.json.shouldEqualJson
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import spark.HaltException
 import spark.Request
 import spark.Response
 import xyz.haff.apisecurity.controller.UserController
 import xyz.haff.apisecurity.database.PermissionsRepository
+import xyz.haff.apisecurity.database.UserRepository
 
 class UserControllerTest : FunSpec({
 
     context("authentication") {
-        test("halts for missing authentication") {
-            val userController = UserController(mockk(), mockk(), mockk())
+
+        test("can register user") {
+            val expectedUsername = "anon"
+            val savedUsername = slot<String>()
+            val userRepository = mockk<UserRepository> {
+                every { save(capture(savedUsername), any()) } returns Unit
+            }
+            val userController = UserController(userRepository, mockk(), Config(inputValidation = false))
 
             val request = mockk<Request> {
-                every { attribute<String>("subject") } returns null
+                every { body() } returns """{ "username": "$expectedUsername", "password": "anywoulddothankyou" }"""
             }
-            val response = mockk<Response>(relaxed = true)
 
-            // I don't really now why, but halt throws this exception
-            shouldThrow<HaltException> {
-                userController.requireAuthentication(request, response)
+            val returnedStatus = slot<Int>()
+            val returnedLocation = slot<String>()
+            val response = mockk<Response> {
+                every { status(capture(returnedStatus)) } returns Unit
+                every { header("Location", capture(returnedLocation))} returns Unit
             }
+
+            val jsonResponse = userController.registerUser(request, response)
+            savedUsername.captured shouldBe expectedUsername
+            returnedStatus.captured shouldBe 201
+            returnedLocation.captured shouldBe "/users/$expectedUsername"
+            jsonResponse.toString() shouldEqualJson """{ "username": "$expectedUsername" }"""
+
         }
 
-        test("proceeds when authenticated") {
-            val userController = UserController(mockk(), mockk(), mockk())
+        context("requiring authentication") {
+            test("halts for missing authentication") {
+                val userController = UserController(mockk(), mockk(), mockk())
 
-            val request = mockk<Request> {
-                every { attribute<String>("subject") } returns "anon"
+                val request = mockk<Request> {
+                    every { attribute<String>("subject") } returns null
+                }
+                val response = mockk<Response>(relaxed = true)
+
+                // I don't really now why, but halt throws this exception
+                shouldThrow<HaltException> {
+                    userController.requireAuthentication(request, response)
+                }
             }
-            val response = mockk<Response>(relaxed = true)
 
-            shouldNotThrowAny {
-                userController.requireAuthentication(request, response)
+            test("proceeds when authenticated") {
+                val userController = UserController(mockk(), mockk(), mockk())
+
+                val request = mockk<Request> {
+                    every { attribute<String>("subject") } returns "anon"
+                }
+                val response = mockk<Response>(relaxed = true)
+
+                shouldNotThrowAny {
+                    userController.requireAuthentication(request, response)
+                }
             }
         }
     }
