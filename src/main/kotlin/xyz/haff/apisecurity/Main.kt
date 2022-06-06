1b7a7910e710a087b33d2ad2d6124361d9220cfc
Haf <haf@protonmail.ch>
2022-06-06 21:02:08 +0200

    fully tested controllers

--- src/main/kotlin/xyz/haff/apisecurity/Main.kt
+++ src/main/kotlin/xyz/haff/apisecurity/Main.kt
@@ -3,7 +3,6 @@ package xyz.haff.apisecurity
 import org.kodein.di.instance
 import org.kodein.di.newInstance
 
-// TODO: I'm sure I can test each controller independently, at the bare minimum
 fun main(args: Array<String>) {
     val di = createInjectionContext()
     val serverConfigurer by di.newInstance { ServerConfigurer(instance(), instance(), instance(), instance()) }
--- src/test/kotlin/xyz/haff/apisecurity/controller/SpaceControllerTest.kt
+++ src/test/kotlin/xyz/haff/apisecurity/controller/SpaceControllerTest.kt
@@ -72,6 +72,23 @@ class SpaceControllerTest : FunSpec({
     }
 
     test("a space is actually created") {
+        // ARRANGE
+        val (spaceId, owner, spaceName) = Tuple3(1L, "owner", "space")
+        val mockSpaceRepository = mockk<SpaceRepository> { every { save(any(), any()) } returns spaceId }
 
+        val spaceController = SpaceController(
+            spaceRepository = mockSpaceRepository,
+            permissionsRepository = mockk(relaxed = true),
+            config = Config(),
+        )
+
+        val request = mockk<Request> { every { body() } returns buildRequest(owner, spaceName) }
+        val response = mockk<Response>(relaxed = true)
+
+        // ACT
+        spaceController.createSpace(request, response)
+
+        // ASSERT
+        verify { mockSpaceRepository.save(spaceName, owner) }
     }
 })
